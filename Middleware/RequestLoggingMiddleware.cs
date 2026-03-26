3e7076ba208a2404e6c06c15101ac6012142c646
zachschool1 <zacharyjordan@my.whccd.edu>
2026-03-25 19:28:48 -0700

    Add project files.

---
+++ Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,19 @@
+﻿namespace COMP003B.Assignment2.Middleware
+{
+    public class RequestLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next; 
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine($"[Request] {context.Request.Method} {context.Request.Path}");
+            await _next(context);
+            Console.WriteLine($"[Response] {context.Response.StatusCode}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+/*Zachary Jordan
+ * COMP003B ASP.NET Core
+ * Faculty name: Jonathan Cruz
+ * The purpose of this application is going to be something simple, maybe it talks about a fake product or something. I haven't decided yet I just want to make sure I get the set up correct
+ */
+
+
+namespace COMP003B.Assignment2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseWelcomePage("/welcome");
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
